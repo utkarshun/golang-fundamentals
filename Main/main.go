ae02415587480c81c83e862047a22692c481ea58
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2025-04-28 17:25:29 +0530

    Add files via upload

---
+++ Main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("##### Welcome to ToDoList #####")
+
+	fmt.Println("List of my Todos")
+	fmt.Println("1.Watch go Crash Course")
+	fmt.Println("2.Watch Nana's Go Crashcourse")
+	fmt.Println("3.i need a treat from your side")
+	fmt.Println("Tutorials")
+	fmt.Println(44)
+}
